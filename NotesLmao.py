5e5e523adb5bb53cf7990f5ca64963fd7d7bfce0
DavidEsketit <31050589+DavidEsketit@users.noreply.github.com>
2017-11-28 15:07:15 -0800

    Adding Previous Notes
    11.27.17

---
+++ NotesLmao.py
@@ -0,0 +1,31 @@
+print("Hello World!")
+
+# David Solis
+
+print(3 + 5)
+print(5 - 3)
+print(5 * 3)
+print(6 / 2)
+print(3 ** 2)
+
+print()
+print("See if you can figure this out")
+print(100 % 3)
+
+# Variables
+car_name = "Wiebe Mobile"
+car_type = "Lamborghini Sesto Elemento"
+car_cylinders = 8
+car_mpg = 9000.1
+
+# Inline Printing
+print("My car is the %s." % car_name)
+print("My car is the %s. It is a %s" % (car_name, car_type))
+
+# Taking input
+name = input("What is your name? ")
+print("Hello %s." % name)
+print(name)
+
+age = input("What is your age? ")
+print("%s is a nice age to be in." % age)
