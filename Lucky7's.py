33bb202880a44e59322e1fc175b2d1c0394f7af1
David Solis <rbpi@wetigers.org>
2018-01-12 15:38:16 -0800

    1.12.18
    Was about to finish, rip.

--- Lucky7's.py
+++ Lucky7's.py
@@ -3,23 +3,23 @@ money = 15
 trial = 0
 print("Welcome to Lucky 7's!")
 
+
 while money > 0:
     most_money = 0
     dice1 = random.randint(1, 6)
     dice2 = random.randint(1, 6)
     number = dice1 + dice2
+    trial += 1
     if number == 7:
         money += 4
-        trial += 1
         print("cOoL")
         print("You have %s dollar(s)left" % money)
     elif number != 7:
         money -= 1
-        trial += 1
         print("dummy")
         print("You have %s dollar(s)left" % money)
-    if money > most_money:
-        most_money + money
+    while money > most_money:
+        most_money = money
 
 
 def no_money():
