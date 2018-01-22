7a9e6c1a288588b739b2087b106b8a9b17795570
David Solis <rbpi@wetigers.org>
2018-01-22 15:42:32 -0800

    1.22.18
    Did some stuff on hangman.

--- Hangman.py
+++ Hangman.py
@@ -9,5 +9,23 @@ Outline of Hangman
 5. Create the win condition
 
 """
+print("You are playing a game of hangman.")
+print("You must guess a word relating to Marvel or DC comics.")
+print("If you guess the letter incorectly 10 times you will lose.")
 word_bank = ["Batman", "Wonderwoman", "Joker", "Deadshot", "The Flash", "Reverse Flash", "Firestorm", "Thor", "Ironman",
              "Captain America"]
+word = random.choice(word_bank)
+guesses_left = 1
+letters_guessed = ""
+print(word)
+while guesses_left > 0:
+    num_of_word = len(word)
+    print("The length of the word is %s" % num_of_word)
+    response = (input("Guess now. "))
+    print(response + letters_guessed)
+    if response == word:
+        guesses_left -= 1
+        print("So far you got %s" % response)
+    elif response != word:
+        guesses_left -= 1
+        print("That's not a letter.")
